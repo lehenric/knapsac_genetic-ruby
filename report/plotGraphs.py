ea56f9287c246db884ee3eda8bb013cf7e5e528b
lehenric <hnrchl@gmail.com>
2019-01-08 15:29:16 +0100

    add detecting first row name of csv

--- report/plotGraphs.py
+++ report/plotGraphs.py
@@ -10,8 +10,12 @@ csv_path = 'csv'
 # get list of files
 only_files = [f for f in listdir(csv_path) if isfile(join(csv_path, f))]
 for csv in only_files:
+    first_line = ''
+    with open(join(csv_path,csv)) as f:
+        first_line = f.readline()
+    first_line=first_line.split(',')[0]
     print(join(csv_path,csv))
-    data_file = Pandas.read_csv(join(csv_path,csv), index_col='generation')
+    data_file = Pandas.read_csv(join(csv_path,csv), index_col=first_line)
     Pyplot.xlabel('No. of generations')
     Pyplot.ylabel('Best fitness')
     plot_title = csv.split('/')[-1].strip('.csv').replace('_',' = ')
