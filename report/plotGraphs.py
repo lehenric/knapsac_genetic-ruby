4c78d8e58776835f9042df7cea6b108c7c38c939
lehenric <hnrchl@gmail.com>
2019-01-09 23:22:37 +0100

    edit plotgraph to name axes according to headers in csv

--- report/plotGraphs.py
+++ report/plotGraphs.py
@@ -13,14 +13,15 @@ for csv in only_files:
     first_line = ''
     with open(join(csv_path,csv)) as f:
         first_line = f.readline()
-    first_line=first_line.split(',')[0]
+    first_col=first_line.split(',')[0]
+    second_col=first_line.split(',')[1]
     print(join(csv_path,csv))
-    data_file = Pandas.read_csv(join(csv_path,csv), index_col=first_line)
+    data_file = Pandas.read_csv(join(csv_path,csv), index_col=first_col)
     Pyplot.figure()
-    Pyplot.xlabel('No. of generations')
-    Pyplot.ylabel('Best fitness')
+    Pyplot.xlabel(first_col.title())
+    Pyplot.ylabel(second_col.title())
     plot_title = csv.split('/')[-1].strip('.csv').replace('_',' = ')
-    Pyplot.title(f'Fitness developement with {plot_title}')
+    Pyplot.title(f'{second_col.title()} developement with {plot_title}')
     Pyplot.plot(data_file)
     Pyplot.savefig(join('png',csv.replace('.csv','.png')))
     # Pyplot.show()
